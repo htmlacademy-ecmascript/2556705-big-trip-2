c8d274a664f2057dc09af2e8481274f2b4bea963
Кириллов Иван <vanyusha_kirillov_03@mail.ru>
2025-10-06 23:16:36 +0300

    Создание main.js и presenter.js
    В header два моих отрисованных элемента поменялись местами
    В main после сортировки вместо трех элементов всегда вставляется один

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import HeaderInfoView from './view/header-info-view.js';
+import FilterView from './view/filter-view.js';
+import {render} from './render.js';
+import BoardPresenter from './presenter/board-presenter.js';
+
+const siteMainEventsElement = document.querySelector('.trip-events');
+const headerInfoElement = document.querySelector('.trip-main');
+const siteFilterElement = document.querySelector('.trip-controls__filters');
+
+const boardPresenter = new BoardPresenter({boardContainer: siteMainEventsElement});
+
+render(new HeaderInfoView(), headerInfoElement);
+render(new FilterView(), siteFilterElement);
+
+boardPresenter.init();
---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,25 @@
+import SortView from '../view/sort-view.js';
+import EventListView from '../view/event-list-view.js';
+import EventEditView from '../view/event-edit-view.js';
+import EventPointView from '../view/event-point-view.js';
+import {render} from '../render.js';
+
+export default class BoardPresenter {
+  eventListComponent = new EventListView();
+  eventEditComponent = new EventEditView();
+  eventPointComponent = new EventPointView();
+
+  constructor({boardContainer}) {
+    this.boardContainer = boardContainer;
+  }
+
+  init() {
+    render(new SortView(), this.boardContainer);
+    render(this.eventListComponent, this.boardContainer);
+    render(this.eventEditComponent, this.eventListComponent.getElement());
+
+    for (let i = 0; i < 1; i++) {
+      render(this.eventPointComponent, this.eventListComponent.getElement());
+    }
+  }
+}
